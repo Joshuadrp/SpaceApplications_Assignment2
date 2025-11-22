8796fc7a733983f3f22936ba33c0801c270d14b6
Joshua <joshuadrp@hotmail.com>
2025-11-22 05:02:12 +0000

    Update code for part 2,3 of q1

--- Q1.py
+++ Q1.py
@@ -1,7 +1,6 @@
 from osgeo import gdal
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.animation as animation
 
 # Suppress GDAL warnings
 gdal.UseExceptions()
@@ -28,7 +27,6 @@ def normalized_burn_ratio(nir, swir):
     """
     return (nir - swir) / (nir + swir)
 nbr = normalized_burn_ratio(numpy_nir, numpy_swir)
-
 print(f"My NBR has a shape of: {nbr.shape}")
 
 #1B Track time evolution of NBR at pixel level according to professor feedback(when and where it happened)
@@ -44,23 +42,44 @@ for ax, t in zip(axes.flat, time):
     ax.axis('off')
 plt.tight_layout()
 plt.show()
+
 #2 Key times in the vegetation monitoring, map of the NBR index of the area at those times
+key_times = [12, 19, 27]
+titles = ['Before Fire', 'During Fire', 'After Fire']
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+for ax, t, title in zip(axes, key_times, titles):
+    ax.imshow(nbr[t], cmap='RdYlGn', vmin=-1, vmax=1)
+    ax.set_title(f'{title} (T{t})')
+    ax.axis('off')
+plt.tight_layout()
+plt.show()
+
+#3 Check the data and warn the observer that something was happening with the vegetation in the forest.
+def forrest_in_trouble(nbr):
+    known_good = nbr[0]
+    total_pixels = nbr.shape[1] * nbr.shape[2]
+    there_is_fire = False
+
+    for t in range(1, nbr.shape[0]):
+        nbr_change = known_good - nbr[t]
+
+        unburned = np.sum(nbr_change < 0.1)
+        low_sev = np.sum((nbr_change >= 0.1) & (nbr_change <= 0.27))
+        moderate = np.sum((nbr_change > 0.27) & (nbr_change <= 0.66))
+        high_sev = np.sum(nbr_change > 0.66)
+
+        unburned_percent = (unburned/total_pixels) * 100
+        low_percent = (low_sev/total_pixels) * 100
+        moderate_percent = (moderate/total_pixels) * 100
+        high_percent = (high_sev/total_pixels) * 100
+
+        if not there_is_fire:
+            if high_percent > 0.1 or moderate_percent > 1.0 or low_percent > 3.0:
+                there_is_fire = True
+                print(f"FIRE DETECTED AT T{t}, TAKE ACTION!!")
 
 
+forrest_in_trouble(nbr)
 
-# # Show which areas changed the most over time
-# std_nbr = nbr.std(axis=0)
-# plt.figure(figsize=(10, 8))
-# im = plt.imshow(std_nbr, cmap='hot')
-# plt.colorbar(im, label='NBR Variability')
-# plt.xlabel('Pixel Column')
-# plt.ylabel('Pixel Row')
-# plt.title('Areas with Most Change Over Time\n(Bright = High change, Dark = Stable)')
-# plt.tight_layout()
-# plt.show()
 
-# Burn Severity Thresholds
-# unburned = np.sum(nbr < 0.1)
-# low_sev = np.sum((nbr >= 0.1) & (nbr <= 0.27))
-# moderate = np.sum((nbr > 0.27) & (nbr <= 0.66))
-# high_sev = np.sum(nbr > 0.66)
